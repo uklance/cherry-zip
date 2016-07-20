f32ccfbcd3687f718292b722220f8ff1ad07c0a0
uklance <uklance@gmail.com>
2016-07-20 19:48:02 +0100

    Bit of tidying up

--- src/main/java/com/lazan/cherryzip/CherryZipMain.java
+++ src/main/java/com/lazan/cherryzip/CherryZipMain.java
@@ -2,15 +2,15 @@ package com.lazan.cherryzip;
 
 import javax.swing.UIManager;
 
-import com.lazan.tinyioc.ServiceModule;
 import com.lazan.tinyioc.ServiceRegistry;
 import com.lazan.tinyioc.ServiceRegistryBuilder;
 
 public class CherryZipMain {
 	public static void main(String[] args) throws Exception {
 		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		ServiceModule[] modules = { new CherryZipModule() };
-		ServiceRegistry registry = new ServiceRegistryBuilder().withModules(modules).build();
+		ServiceRegistry registry = new ServiceRegistryBuilder()
+				.withModuleType(CherryZipModule.class)
+				.build();
 		MainFrame mainFrame = registry.getService(MainFrame.class);
 		mainFrame.setVisible(true);
 	}
